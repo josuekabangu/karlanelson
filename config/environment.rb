e249a11a0b48c69a910e8c1c8f98fc8848b11e06
josuekabangu <ajkabs2@gmail>
2021-09-12 13:28:23 +0200

    gmail settings

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,21 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
+#ActionMailer::Base.smtp_settings = {
+#  :user_name => ENV['SENDGRID_LOGIN'],
+#  :password => ENV['SENDGRID_PWD'],
+#  :domain => 'monsite.fr',
+#  :address => 'smtp.sendgrid.net',
+#  :port => 587,
+#  :authentication => :plain,
+#  :enable_starttls_auto => true
+#}
+
 ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['SENDGRID_LOGIN'],
-  :password => ENV['SENDGRID_PWD'],
-  :domain => 'monsite.fr',
-  :address => 'smtp.sendgrid.net',
+  :user_name => ENV['GMAIL_LOGIN'],
+  :password => ENV['GMAIL_PWD'],
+  :address => 'smtp.gmail.com',
+  :domain => 'google.com',
   :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
+  :authentication => :plain
 }
